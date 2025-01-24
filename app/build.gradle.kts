3c47831a8ca333839d975e9b596912c94f6ad556
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2025-01-25 06:39:58 +0900

    [0125] Refactor: TimelineActivity를 TimelineFragment로 분리
    - RecyclerView와 관련된 로직을 Fragment로 이동
    - BottomNavigationView는 Activity에서 관리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
+    // ViewModel
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
 }
